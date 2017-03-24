1029dcdbae04f91dd6812830aa3a1c8cc04464a2
kenpeter <figo2476@hotmail.com>
2017-03-24 15:02:07 +1100

    update

--- osmosis_hlj.js
+++ osmosis_hlj.js
@@ -33,53 +33,50 @@ function run() {
     return new Promise((resolve, reject) => {
       osmosis
         .get(list)
-        .find('.item .box')
-        .set({
-          'title': '.title h2 a',
-          'idTitle': '.title h2 a@title',
-          'price': '.ourbox .price',
-        })
         .find('.title h2 a')
         .follow('@href')
         .set({
-          'imgs': ['img@src'],
+          'title': 'h1.product-title',
+          'price': 'span.sale-price',
+          'imgs': ['img@src']
         })
         .data((mydata) => {
           let productObj = {};
 
           let title = mydata.title;
           let price = mydata.price;
-
-          let idTitle = mydata.idTitle;
-          let tmpArr = idTitle.split('/');
-          let productId = tmpArr[tmpArr.length-1];
-          let productUrl = idTitle.replace(/\/\//, '');
-
           let imgs = mydata.imgs;
-
-          console.log('---------- single page ----------');
-          console.log(title);
-          console.log(price);
-          console.log(productId);
-          console.log(productUrl);
+          let ProductId = null;
 
           productObj = {
             title: title,
             price: price,
-            productId: productId,
-            productUrl: productUrl,
+            productId: null,
+            productUrl: null,
             imgs: []
           };
 
-          Promise.each(imgs, (img) => {
+          // each image
+          let isAssignProductId = true;
+          Promise.each(imgs, (img, index) => {
             return new Promise((resolve1, reject1) => {
-              //console.log(img);
               if(img.includes('cloudfront')) {
                 if(img.includes('thumb_')) {
                   let tmpImg = img.replace('thumb_', '');
                   tmpImg = tmpImg.replace(/\/\//, '');
-                  //console.log(tmpImg);
-                  productObj.imgs.push(tmpImg);
+                  let fileName = getImageFilename(tmpImg);
+
+                  // Hak
+                  let singImgUrl = 'http://d3toummn8j74h.cloudfront.net/ban/' + fileName;
+                  productObj.imgs.push(singImgUrl);
+                  if(isAssignProductId) {
+                    let tmpProdId = getProductIdByPath(tmpImg);
+                    productObj.productId = tmpProdId;
+                    productObj.productUrl = 'http://hlj.com/product/' + tmpProdId;
+                    isAssignProductId = false;
+                  }
+
+
                 }
                 else {
 
@@ -88,63 +85,50 @@ function run() {
               else {
 
               }
-              // resolve no matter what
+
               resolve1();
+
             });
           })
           .then(() => {
-            console.log('promise.each done');
-
-            // save
+            // Save to db
             productDAO
               .save(productObj)
               .then(() => {
-                // Now write to file
-                Promise.each(productObj.imgs, (imgUrl) => {
-                  return new Promise((resolve2, reject2) => {
-                    // imgUrl === d3toummn8j74h.cloudfront.net/ban/ban965506_box_1485108727.jpg?v=1485108727
-                    let theImgUrl = 'http://' + imgUrl;
-                    axios
-                      .get(theImgUrl, {responseType: 'arraybuffer'})
-                      .then((imgData) => {
-                        let tmpArr = imgUrl.split('/');
-                        let lastElement = tmpArr[tmpArr.length-1];
-                        let fileNameArr = lastElement.split('?');
-                        let fileName = fileNameArr[0];
+                console.log('---------- single page ----------');
+                //console.log(productObj);
+                console.log('productObj is saved@');
 
-                        let productImgDir = path.resolve(__dirname, 'imgs', productId);
-                        if (!fs.existsSync(productImgDir)) {
-                          fs.mkdirSync(productImgDir);
-                        }
-
-                        let savePath = path.resolve(__dirname, 'imgs', productId, fileName);
-
-                        fs.writeFile(savePath, imgData.data, "binary", (err) => {
-                          if(err) {
-                            console.log(err);
-                          }
-                          else {
-                            console.log(`save: ${savePath}`);
-                            resolve2();
-                          }
-                        });
-
-                      });
-                  });
-                })
-                .then(() => {
-                  console.log('all image save for a product');
-                  resolve();
-                });
-
-
+                resolve()
               });
 
           });
-        });
-    });
-  });
+        })
+        .error((err) => {
+          console.error(err);
+          reject();
+        }); // end data
+      }); // end promise
 
+    }); // end promise each
+}
+
+
+function getImageFilename(path) {
+  //e.g. d3toummn8j74h.cloudfront.net/ban/bann12185_0_1482294330.jpg?v=1482294330
+  let arr = path.split('/');
+  let lastElement = arr[arr.length-1];
+  let fileName = lastElement.split('?')[0];
+  return fileName;
+}
+
+function getProductIdByPath(path) {
+  //e.g. d3toummn8j74h.cloudfront.net/ban/bann12185_0_1482294330.jpg?v=1482294330
+  let arr = path.split('/');
+  let lastElement = arr[arr.length-1];
+  let fileName = lastElement.split('?')[0];
+  let productId = fileName.split('_')[0];
+  return productId;
 }
 
 // run
